e8a4a9276c43d48ab96f5eaf2f5cf2b5da295bc3
BryQ47 <m.bryk@onet.pl>
2016-05-02 12:07:32 +0200

    Cleanup

--- StudentsListJS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StudentsListJS.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- StudentsListJS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StudentsListJS.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
